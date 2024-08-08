a530c1ab171ad7e09e24071b64d1799c68d9c1ee
Dileep Nagendra <idileepd@gmail.com>
2024-08-08 22:33:01 +0530

    added remove

--- python-mqtt-hello/scripts/remove_local.sh
+++ python-mqtt-hello/scripts/remove_local.sh
@@ -1,3 +1,3 @@
-sudo rm /greengrass/v2/logs/com.example.PythonMqttHello.log
-sudo touch /greengrass/v2/logs/com.example.PythonMqttHello.log
+# sudo rm /greengrass/v2/logs/com.example.PythonMqttHello.log
+# sudo touch /greengrass/v2/logs/com.example.PythonMqttHello.log
 sudo /greengrass/v2/bin/greengrass-cli deployment create --remove "com.example.PythonMqttHello"
