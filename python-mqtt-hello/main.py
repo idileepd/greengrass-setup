e89df31ce62f74f244095b5625f0b1cb7875ea13
Dileep Nagendra <idileepd@gmail.com>
2024-08-08 22:29:26 +0530

    revert

--- python-mqtt-hello/main.py
+++ python-mqtt-hello/main.py
@@ -1,28 +1,16 @@
 import sys
 import time
-import datetime
+from datetime import datetime
 from src.hello_client import HelloClient
 
 
 def main():
-    print("\n\n\n::::: MAIN Started")
-    # try:
-    #     args = sys.argv[1:]
-    #     client = HelloClient(args[0])
-
-    #     print(">>>>> About to sub")
-    #     # Subscribe to the 'hello' topic
-    #     # client.subscribe()
-
-    #     times = 20
-    #     print("Started ::::", datetime.datetime.now(tz=None))
-    #     while times > 0:
-    #         client.tick()
-    #         time.sleep(5)
-    #         times = times - 1
-    # except Exception as e:
-    #     print("ERRORRR :::::: \n\n")
-    #     print(e)
+    args = sys.argv[1:]
+    client = HelloClient(args[0])
+    print(":::::", datetime.now())
+    while True:
+        client.tick()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
--- python-mqtt-hello/src/hello_client.py
+++ python-mqtt-hello/src/hello_client.py
@@ -1,37 +1,12 @@
 from awsiot import greengrasscoreipc
 from awsiot.greengrasscoreipc import model
 import json
-from time import gmtime, strftime
 
 
 class HelloClient:
     def __init__(self, message: str):
         self._client = greengrasscoreipc.connect()
-        self._message = json.dumps(
-            {"message": message, "time": strftime("%Y-%m-%d %H:%M:%S", gmtime())}
-        )
-        self._subscription = None
-
-    def subscribe(self):
-        # Create a stream handler function
-        def stream_handler(event: model.SubscribeToIoTCoreResponse):
-            print(":::::Received message from 'hello' topic:", event.payload.decode())
-
-        # Create a subscription to the 'hello' topic
-        self._subscription = self._client.new_subscribe_to_iot_core()
-        self._subscription.activate(
-            model.SubscribeToIoTCoreRequest(
-                topic_name="hello", qos=model.QOS.AT_LEAST_ONCE
-            ),
-            stream_handler=stream_handler,
-        )
-
-        # Handle the subscription response
-        try:
-            response = self._subscription.get_response().result(timeout=5.0)
-            print(":::::Successfully subscribed to 'hello' topic.", response)
-        except Exception as e:
-            print("Failed to subscribe to 'hello' topic:", e)
+        self._message = json.dumps({"message": message})
 
     def tick(self):
         op = self._client.new_publish_to_iot_core()
@@ -44,6 +19,6 @@ class HelloClient:
         )
         try:
             result = op.get_response().result(timeout=5.0)
-            print("::::Successfully published message:", result)
+            print("Successfully published message:", result)
         except Exception as e:
-            print(":::::Failed to publish message:", e)
+            print("Failed to publish message:", e)
