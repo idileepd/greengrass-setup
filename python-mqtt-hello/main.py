248707c517b5eaf52db1d315ac3c70cfaf542314
Dileep Nagendra <idileepd@gmail.com>
2024-08-08 16:21:16 +0530

    added main

--- python-mqtt-hello/main.py
+++ python-mqtt-hello/main.py
@@ -1,6 +1,6 @@
 import sys
 import time
-
+import datetime
 from src.hello_client import HelloClient
 
 
@@ -8,6 +8,7 @@ def main():
     args = sys.argv[1:]
     client = HelloClient(args[0])
     times = 5
+    print("Started ::::", datetime.datetime.now(tz=None))
 
     while times > 0:
         client.tick()
