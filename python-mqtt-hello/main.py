2b467235652839f30ec83f1b3ac46e888347edac
Dileep Nagendra <idileepd@gmail.com>
2024-08-08 22:17:31 +0530

    remove all other code just pring

--- python-mqtt-hello/main.py
+++ python-mqtt-hello/main.py
@@ -6,23 +6,23 @@ from src.hello_client import HelloClient
 
 def main():
     print("\n\n\n::::: MAIN Started")
-    try:
-        args = sys.argv[1:]
-        client = HelloClient(args[0])
+    # try:
+    #     args = sys.argv[1:]
+    #     client = HelloClient(args[0])
 
-        print(">>>>> About to sub")
-        # Subscribe to the 'hello' topic
-        # client.subscribe()
+    #     print(">>>>> About to sub")
+    #     # Subscribe to the 'hello' topic
+    #     # client.subscribe()
 
-        times = 20
-        print("Started ::::", datetime.datetime.now(tz=None))
-        while times > 0:
-            client.tick()
-            time.sleep(5)
-            times = times - 1
-    except Exception as e:
-        print("ERRORRR :::::: \n\n")
-        print(e)
+    #     times = 20
+    #     print("Started ::::", datetime.datetime.now(tz=None))
+    #     while times > 0:
+    #         client.tick()
+    #         time.sleep(5)
+    #         times = times - 1
+    # except Exception as e:
+    #     print("ERRORRR :::::: \n\n")
+    #     print(e)
 
 
 if __name__ == "__main__":
